05f6836ee7e4afd7f9e3f57963c3960dbd3ddc22
Thomas Dubiel <td@crazynexus.de>
2017-06-15 15:23:05 +0200

    initial Commit with a simple ListView and a Custom Adapter using a custom cell layout

---
+++ listViewDemo/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Util;
+
+namespace listViewDemo
+{
+	[Activity(Label = "listViewDemo", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			var data = new List<string>();
+			for (var i = 0; i < 100; i++)
+			{
+				data.Add("Item number: " + i.ToString());
+			}
+
+			var listView = FindViewById<ListView>(Resource.Id.listView1);
+			listView.FastScrollEnabled = true;
+
+			// Simple Adapter using with standard stuff
+			// var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, data);
+
+			var adapter = new MyAdapter(this, data);
+
+			listView.Adapter = adapter;
+			listView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) =>
+			{
+				Log.Debug("ITEM CLICKED", "Number: " + e.Position);
+			};
+		}
+	}
+}
+
---
+++ listViewDemo/MyAdapter.cs
@@ -0,0 +1,71 @@
+﻿//
+// MyAdapter.cs
+//
+// Created by Thomas Dubiel on 15.06.2017
+// Copyright 2017 Thomas Dubiel. All rights reserved.
+//
+using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+
+namespace listViewDemo
+{
+	public class MyAdapter : BaseAdapter<string>
+	{
+		List<string> items;
+		Activity context;
+
+		public MyAdapter(Activity context, List<string> items)
+		{
+			this.items = items;
+			this.context = context;
+		}
+
+		public override string this[int position]
+		{
+			get
+			{
+				return items[position];
+			}
+		}
+
+		public override int Count
+		{
+			get
+			{
+				return items.Count;
+			}
+		}
+
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
+
+		public override View GetView(int position, View convertView, ViewGroup parent)
+		{
+			View view = convertView;
+			if (view == null)
+				view = context.LayoutInflater.Inflate(Resource.Layout.CustomRow, null);
+
+			var item = items[position];
+
+			var clock = view.FindViewById<AnalogClock>(Resource.Id.analogClockCustomRow);
+			if (position == 3)
+			{
+				item = "(Available for subscribers only)";
+				clock.SetBackgroundColor(new Android.Graphics.Color(200, 55, 55));
+			}
+			else
+			{
+				clock.SetBackgroundColor(new Android.Graphics.Color(55, 55, 200));
+			}
+
+
+			view.FindViewById<TextView>(Resource.Id.textViewCustomRow).Text = item;
+			return view;
+		}
+	}
+}
